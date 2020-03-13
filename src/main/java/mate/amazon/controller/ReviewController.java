4378d5a57a7f49faa382de2c58114770204ac9dc
Riptod <otoval15>
2020-03-13 11:25:31 +0200

    add service

--- src/main/java/mate/amazon/controller/ReviewController.java
+++ src/main/java/mate/amazon/controller/ReviewController.java
@@ -5,40 +5,34 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import mate.amazon.entity.AmazonReviewEntity;
-import mate.amazon.repository.AmazonRepository;
-import mate.amazon.service.FindMostFrequentWordFromComments;
+import mate.amazon.service.ReviewService;
 import mate.amazon.utils.CustomCsvParser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import static org.hibernate.bytecode.BytecodeLogger.LOGGER;
 
 @RestController
 public class ReviewController {
-    private final AmazonRepository amazonRepository;
-    private final CustomCsvParser customCsvParser;
-    private final FindMostFrequentWordFromComments findMostFrequentWordFromComments;
 
-    public ReviewController(AmazonRepository amazonRepository, CustomCsvParser customCsvParser,
-                            FindMostFrequentWordFromComments findMostFrequentWordFromComments) {
-        this.amazonRepository = amazonRepository;
-        this.customCsvParser = customCsvParser;
-        this.findMostFrequentWordFromComments = findMostFrequentWordFromComments;
-    }
+    @Autowired
+    private ReviewService reviewService;
+    @Autowired
+    private CustomCsvParser customCsvParser;
 
     @GetMapping("/users")
     public List<String> getMostActiveUsers() {
-        return amazonRepository.findActiveUsers(1000);
+        return reviewService.findActiveUsers(1000);
     }
 
     @GetMapping("/mostComments")
     public List<String> getMostCommentedGoods() {
-        return amazonRepository.findMostCommentedGoods(1000);
+        return reviewService.findMostCommentedGoods(1000);
     }
 
     @GetMapping("/popularWords")
     public List<String> getPopularWords() {
-        return findMostFrequentWordFromComments.countWordsInString(
-                amazonRepository.getAllComments());
+        return reviewService.countWordsInString(reviewService.getAllComments());
     }
 
     @PostConstruct
@@ -48,7 +42,7 @@ public class ReviewController {
             List<AmazonReviewEntity> reviews = customCsvParser.readCsvFile("Reviews.csv");
             LOGGER.info((System.currentTimeMillis() - startReading) * 0.001);
             long startSaving = System.currentTimeMillis();
-            amazonRepository.saveAll(reviews);
+            reviewService.saveAll(reviews);
             LOGGER.info((System.currentTimeMillis() - startSaving) * 0.001);
         } catch (IOException e) {
             throw new RuntimeException("Can`t read file", e);
--- src/main/java/mate/amazon/service/impl/ReviewServiceImpl.java
+++ src/main/java/mate/amazon/service/impl/ReviewServiceImpl.java
@@ -5,11 +5,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import mate.amazon.service.FindMostFrequentWordFromComments;
+import mate.amazon.entity.AmazonReviewEntity;
+import mate.amazon.repository.AmazonRepository;
+import mate.amazon.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FindMostFrequentWordFromCommentsImpl implements FindMostFrequentWordFromComments {
+public class ReviewServiceImpl implements ReviewService {
+
+    @Autowired
+    private AmazonRepository amazonRepository;
+
     @Override
     public List<String> countWordsInString(List<String> comments) {
         Map<String, Integer> countWords = new HashMap<String, Integer>();
@@ -30,5 +37,25 @@ public class FindMostFrequentWordFromCommentsImpl implements FindMostFrequentWor
                 .map(Map.Entry::getKey)
                 .limit(1000)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> findActiveUsers(Integer count) {
+        return amazonRepository.findActiveUsers(count);
+    }
+
+    @Override
+    public List<String> findMostCommentedGoods(Integer count) {
+        return amazonRepository.findMostCommentedGoods(count);
+    }
+
+    @Override
+    public List<String> getAllComments() {
+        return amazonRepository.getAllComments();
+    }
+
+    @Override
+    public void saveAll(List<AmazonReviewEntity> reviewEntities) {
+        amazonRepository.saveAll(reviewEntities);
     }
 }
