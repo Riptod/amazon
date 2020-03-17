c1bced3b22f5cb7d4f8b410df46610e237cf89ca
Riptod <otoval15>
2020-03-17 22:56:03 +0200

    add jwt

--- src/main/java/mate/amazon/security/CustomUserDetailsService.java
+++ src/main/java/mate/amazon/security/CustomUserDetailsService.java
@@ -8,10 +8,10 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
-public class CustomUserDetailsServer implements UserDetailsService {
+@Component("customUserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserService userService;
